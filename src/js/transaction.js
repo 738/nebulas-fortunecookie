0031e652d311850ae263afec982697a6e63dcd5c
junwooji <junwoo@finda.co.kr>
2018-06-03 17:34:51 +0900

    add warning snackbar

--- src/js/transaction.js
+++ src/js/transaction.js
@@ -1,8 +1,12 @@
 $(function () {
     $(".pulse").click(function () {
-        var contractDataController = new ContractDataController();
-        contractDataController.sendTransaction(0, "crackCookie", "", pendingCallback, successCallback, failCallback);
-        
+        if (typeof (webExtensionWallet) === 'undefined') {
+            crackCookie();
+        } else {
+            var contractDataController = new ContractDataController();
+            contractDataController.sendTransaction(0, "crackCookie", "", pendingCallback, successCallback, failCallback);
+        }
+
     });
 });
 
@@ -16,9 +20,10 @@ function pendingCallback() {
 
 async function successCallback() {
     var contractDataController = new ContractDataController();
-    await contractDataController.callSmartContract("getHistory", "[]", function(tx) {
+    await contractDataController.callSmartContract("getHistory", "", function (tx) {
         var history = JSON.parse(tx.result);
         var lastFortune = history[history.length - 1].fortune;
+        console.log(tx);
         console.log(history);
         $('#fortune-message-span').html(lastFortune);
     });
@@ -29,4 +34,17 @@ async function successCallback() {
 function failCallback() {
     $("#loading-wrapper").addClass("hide");
     alert('transaction is failed!');
+}
+
+async function crackCookie() {
+    var contractDataController = new ContractDataController();
+    await contractDataController.callSmartContract("crackCookie", "", function (tx) {
+        // var history = JSON.parse(tx.result);
+        var fortune = tx.result;
+        console.log(tx.result);
+        // console.log(history);
+        $('#fortune-message-span').html(fortune.replace(/\"/g, ""));
+    });
+    // $("#loading-wrapper").addClass("hide");
+    showFortune();
 }
